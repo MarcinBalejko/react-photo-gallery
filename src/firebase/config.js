f92fbeebe713b9cd4fed11b1eb7c5ec7a6b30a3f
MarcinBalejko <mbbm9@o2.pl>
2020-08-03 19:08:13 +0200

    :package: Imported Firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,16 @@
+import * as firebase from "firebase/app";
+import "firebase/storage";
+import "firebase/firestore";
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyBcd_w569KhLzbghITLmAbrpIRk-p4yTq4",
+  authDomain: "react-photo-gallery-d3e73.firebaseapp.com",
+  databaseURL: "https://react-photo-gallery-d3e73.firebaseio.com",
+  projectId: "react-photo-gallery-d3e73",
+  storageBucket: "react-photo-gallery-d3e73.appspot.com",
+  messagingSenderId: "487724545290",
+  appId: "1:487724545290:web:639897a227c9f09915b6f3",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
